a94a98071e386b35394151bd3859544498e5f7c3
Ming Liu <ming.codes@gmail.com>
2026-05-14 22:21:59 -0700

    refactor: migrate tailwindcss scraper to updated Docset API

--- bin/tailwindcss/4.2/index.ts
+++ bin/tailwindcss/4.2/index.ts
@@ -1,8 +1,5 @@
 import { Docset } from "@/docset";
 import { guid, metaFrom } from "@/utils";
-import { findAll, type Url } from "css-tree";
-import { dirname, join, relative } from "node:path";
-import { fileURLToPath, pathToFileURL } from "node:url";
 
 const docset = new Docset("tailwindcss");
 
@@ -15,64 +12,27 @@ await docset.scaffold();
 await docset.crawl(async ({ navbar }) => {
   const links = await navbar(new URL("https://tailwindcss.com/docs/"));
 
-  return links.slice(-2);
-});
-
-await docset.documents((document, file) => {
-  const url = new URL(document.location.href);
-
-  Array.from(document.querySelectorAll('link[rel="stylesheet"]'), (sheet) => {
-    const sheetFilePath = docset.resolveFile(
-      join(sheet.getAttribute("href")!, "content.css"),
-      file,
-    );
-
-    sheet.setAttribute(
-      "href",
-      decodeURIComponent(relative(dirname(url.href), sheetFilePath.href)),
-    );
+  return links.filter((link) => {
+    return link.pathname.startsWith("/docs");
   });
-});
-
-await docset.stylesheets(async (ast, file) => {
-  const fontFaceAtRules = findAll(ast, (node) => {
-    return node.type === "Atrule" && node.name === "font-face";
-  });
-
-  const nodes = fontFaceAtRules.flatMap((ast) => {
-    return findAll(ast, (node) => {
-      return node.type === "Url" && !node.value.startsWith("data:");
-    });
-  });
-
-  await Promise.all(
-    nodes
-      .filter((node): node is Url => node.type === "Url")
-      .map(async (node) => {
-        const dirPath = fileURLToPath(
-          docset.resolveFile(node.value, new URL(dirname(file.href))),
-        );
-        const meta = await metaFrom(pathToFileURL(join(dirPath, "meta.json")));
-        const target = join(dirPath, `content.${meta.extname}`);
-
-        node.value = relative(dirname(fileURLToPath(file)), target);
-        // node.value = await base64EncodeFileContent(
-        //   pathToFileURL(target),
-        //   meta.mimeType,
-        // );
-      }),
-  );
 });
 
 await docset.documents((document) => {
-  document.querySelectorAll('[rel="preload"], script').forEach((node) => {
-    node.remove();
-  });
+  document
+    .querySelectorAll('[rel="preload"], script, [rel="manifest"]')
+    .forEach((node) => {
+      node.remove();
+    });
 
   document.querySelectorAll("h1,h2,h3,h4,h5,h6").forEach((node) => {
     if (!node.id) {
       node.id = `${node.textContent.trim().toLowerCase()}-${guid(document)}`;
     }
+
+    const anchor = document.createElement("a");
+
+    anchor.setAttribute("name", `//apple_ref/cpp/Example/stroke-width`);
+    anchor.setAttribute("class", "dashAnchor");
   });
 
   document.querySelectorAll("#quick-reference tbody tr").forEach((node) => {
@@ -90,6 +50,13 @@ await docset.documents((document, _file, meta) => {
   const section = document.querySelector("[data-section]")?.textContent ?? "";
   const h1 = document.querySelector("h1")!;
 
+  document.documentElement.insertBefore(
+    document.createComment(`Online page at ${meta.source}`),
+    document.head,
+  );
+
+  console.log("Indexing...");
+
   if (
     ["getting started", "core concepts"].includes(section.trim().toLowerCase())
   ) {
@@ -97,6 +64,19 @@ await docset.documents((document, _file, meta) => {
       insert(h1.textContent, "Guide", `${meta.localContentPath}#${h1.id}`);
     });
   } else {
+    const ref = document.querySelector("#quick-reference")!;
+
+    if (!ref) {
+      return;
+    }
+
+    docset.toc(ref, "Section", "Quick Reference");
+    docset.toc(document.querySelector("#examples")!, "Section", "Examples");
+
+    document.querySelectorAll("h3").forEach((node) => {
+      docset.toc(node, "Guide");
+    });
+
     docset.prepareSqlIndex((insert) => {
       const tr = document.querySelectorAll("#quick-reference tbody tr");
 
